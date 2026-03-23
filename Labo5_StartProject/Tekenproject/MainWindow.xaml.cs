274706846389b6db9aef28cf38255518a9427644
Jarno Willems <jarnowillems001@gmail.com>
2026-03-23 21:05:35 +0100

    Labo 5 startcode added

---
+++ Labo5_StartProject/Tekenproject/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using Tekenproject.Pattern;
+
+namespace Tekenproject
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DrawLogic receiver;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            receiver = new DrawLogic(this);
+            TekenCanvas.Focus();
+        }
+
+        #region MouseEvents       
+        private void Canvas_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            TekenCanvas.CaptureMouse();
+            receiver.MouseDown(e.GetPosition(TekenCanvas));
+        }
+
+        private void Canvas_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            TekenCanvas.ReleaseMouseCapture();
+            receiver.MouseUp(e.GetPosition(TekenCanvas));
+        }
+
+        #endregion
+        private void Canvas_KeyDown(object sender, KeyEventArgs e)
+        {
+           if (e.Key == Key.F)
+                receiver.StrokeNr++;
+           
+        }
+
+
+        private void BtnFill_Click(object sender, RoutedEventArgs e)
+        {
+            receiver.FillNr++;
+        }
+
+        private void BtnStroke_Click(object sender, RoutedEventArgs e)
+        {
+            receiver.StrokeNr++;
+        }
+
+        private void BtnReset_Click(object sender, RoutedEventArgs e)
+        {
+            receiver.ClearCanvas();
+        }
+
+        private void BtnUndo_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void BtnRedo_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Labo5_StartProject/Tekenproject/Pattern/DrawLogic.cs
@@ -0,0 +1,87 @@
+﻿using System.Reflection;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace Tekenproject.Pattern
+{
+    /*
+     * Receiver
+     * */
+    public class DrawLogic
+    {
+        SolidColorBrush[] colors = { Brushes.Black, Brushes.White, Brushes.Gray, Brushes.Red, Brushes.Green, Brushes.Blue, Brushes.Yellow, Brushes.Purple, Brushes.Brown, Brushes.Orange };
+
+        Random random = new Random();
+
+        MainWindow window;
+        int strokeNr = 0, fillNr = 1;
+
+        Point start, einde;
+
+        public DrawLogic(MainWindow window)
+        {
+            this.window = window;
+        }
+        
+        internal SolidColorBrush StrokeColor => colors[StrokeNr];
+        internal SolidColorBrush FillColor => colors[FillNr];
+
+        internal int StrokeNr
+        {
+            get { return strokeNr; }
+            set
+            {
+                strokeNr = value % colors.Length;
+                window.BrushStroke.Color = StrokeColor.Color;
+            }
+        }
+
+        internal int FillNr
+        {
+            get { return fillNr; }
+            set
+            {
+                fillNr = value % colors.Length;
+                window.BrushFill.Color = FillColor.Color;
+            }
+        }
+        internal void ClearCanvas()
+        {
+            window.TekenCanvas.Children.Clear();
+        }
+
+        internal void AddDrawing(Shape shape)
+        {
+            window.TekenCanvas.Children.Add(shape);
+        }
+
+        internal void MouseDown(Point point)
+        {
+           start = point;
+        }
+
+        internal void MouseUp(Point point)
+        {
+            einde = point;
+
+            einde = point;
+            AddDrawing(GetShape());
+        }
+
+             
+        private Shape GetShape()
+        {
+            Ellipse ellipse = new Ellipse();
+            ellipse.Width = Math.Abs(einde.X - start.X);
+            ellipse.Height = Math.Abs(einde.Y - start.Y);            
+            ellipse.Fill = FillColor;
+            ellipse.Stroke = StrokeColor;
+            Canvas.SetLeft(ellipse, Math.Min(start.X, einde.X));
+            Canvas.SetTop(ellipse, Math.Min(start.Y, einde.Y));
+            return ellipse;
+        }        
+    }
+
+}
