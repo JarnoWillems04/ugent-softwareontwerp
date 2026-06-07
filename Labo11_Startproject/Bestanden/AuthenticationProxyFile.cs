6c26ff9b3293c1dcfce957ae59e3161cf0affaec
Jarno Willems <jarnowillems001@gmail.com>
2026-06-07 20:09:35 +0200

    Labo 11 deel 2 gedaan

---
+++ Labo11_Startproject/Bestanden/AuthenticationProxyFile.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bestanden
+{
+    public class AuthenticationProxyFile : IFile
+    {
+        IFile file;
+
+        public AuthenticationProxyFile(User user, string file)
+        {
+            if (!user.isAdmin)
+            {
+                // womp womp
+            }
+            this.file = new CachingProxyFile(file);
+        }
+
+        public string Content
+        {
+            get
+            {
+                return this.file.Content;
+            }
+        }
+    }
+}
---
+++ Labo11_Startproject/Bestanden/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bestanden
+{
+    public class User
+    {
+        bool isAdmin { get; set; }
+
+        public User(bool isAmdin)
+        {
+            this.isAdmin = isAmdin;
+        }
+    }
+}
