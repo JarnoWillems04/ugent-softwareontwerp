495ea7e3db0654597b88bc96851170f53b7ddde5
Jarno Willems <jarnowillems001@gmail.com>
2026-06-07 19:08:33 +0200

    Labo 10 Added

---
+++ Labo10_Startproject/BibliotheekConsole/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using BibliotheekConsole;
+using Catalogus;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Dummy();
+        YamlBibliotheek();
+        void YamlBibliotheek()
+        {
+            YamlBibliotheek bib = new();
+            IBibItem start = bib.Bibliotheek;
+            Console.WriteLine(start.Toon(0));
+
+        }
+        void Dummy()
+        {
+            DummyBibliotheek bib = new();
+            IBibItem start = bib.Bibliotheek;
+            Console.WriteLine(start.Toon(0));
+
+            IBibItem item = start.Zoek("ID07");
+            Console.WriteLine(item.Toon(0) + "\n");
+
+            Console.WriteLine("ZoekTrefwoord:");
+      
+            foreach (IBibItem ib in start.ZoekTrefwoord("en"))
+            {
+                Console.WriteLine(ib.Toon(0));
+            }
+        }
+    }
+}
---
+++ Labo10_Startproject/Catalogus/ABibComposite.cs
@@ -0,0 +1,67 @@
+﻿
+namespace Catalogus
+{
+    public abstract class ABibComposite : ABibItem
+    {
+        protected ICollection<IBibItem> elementen;
+
+        public override string Toon(int insprong)
+        {
+            string toon = "";
+            for (int i = 0; i < insprong; i++)
+            {
+                toon += "-";
+            }
+            toon += Inhoud+": \n";
+            foreach(IBibItem bibitem in elementen)
+            {
+                toon += bibitem.Toon(insprong + 2) + " \n";
+            }
+
+            return toon;
+        }
+        public override void Verwijder(IBibItem bibItem)
+        {
+            if (elementen.Contains(bibItem))
+            {
+                elementen.Remove(bibItem);
+                bibItem.Ouder = null;
+            }
+        }
+        public override void VoegToe(IBibItem bibItem)
+        {
+            elementen.Add(bibItem);
+            bibItem.Ouder = this;
+        }
+        public override IBibItem Zoek(string id)
+        {
+            IBibItem item= base.Zoek(id);
+            if (item == null) //niet gevonden
+            {
+                foreach (IBibItem bibitem in elementen)
+                {
+                    IBibItem iitem = bibitem.Zoek(id);
+                    if (iitem != null) return iitem;
+                }
+            }
+            return item;
+        }
+        public override IEnumerable<IBibItem> ZoekTrefwoord(string trefwoord)
+        {
+            if (HasTrefwoord(trefwoord))
+            {
+                yield return this;
+            }
+            foreach (IBibItem item in elementen)
+            {
+                {
+                    foreach (IBibItem bibitem in item.ZoekTrefwoord(trefwoord))
+                        yield return bibitem;
+
+                }
+            }
+
+        }
+
+    }
+}
